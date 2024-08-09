d01934ab3349a694729cc6bde597cf425508d4ce
Merlina <noahchukita@gmail.com>
2024-08-08 22:22:26 -0300

    chore: actualiza variables de entorno

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,6 +5,9 @@ export const PORT: number = Number(process.env.PORT) || 3000;
 export const DB_NAME: string = process.env.DB_NAME || "";
 export const DB_USER: string = process.env.DB_USER || "";
 export const DB_PASSWORD: string = process.env.DB_PASSWORD || "";
-export const DB_HOST: string = process.env.DB_HOST || "localhost";
+export const DB_HOST: string = process.env.DB_HOST || "";
 export const DB_PORT: number = Number(process.env.DB_PORT) || 5432;
 export const JWT_SECRET: string = process.env.JWT_SECRET || "secret";
+
+// Añade esta nueva variable para la URL de la base de datos
+export const DATABASE_URL: string = process.env.DATABASE_URL || "";
