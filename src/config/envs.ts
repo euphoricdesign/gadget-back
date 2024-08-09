211a164e8741b4b0740b230186ca1d929c2cb4f5
Merlina <noahchukita@gmail.com>
2024-08-08 21:13:21 -0300

    chore: actualiza variable de entorno

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const PORT: number = Number(process.env.PORT);
+export const PORT: number = Number(process.env.PORT) || 3000;
 export const DB_NAME: string = process.env.DB_NAME || "";
 export const DB_USER: string = process.env.DB_USER || "";
 export const DB_PASSWORD: string = process.env.DB_PASSWORD || "";
