c0234de7405a6a33289ef5974904496c7d90d0cd
Merlina <noahchukita@gmail.com>
2024-08-08 21:03:46 -0300

    build: crea carpeta dist generada por build

---
+++ dist/helpers/preLoadProducts.js
@@ -0,0 +1,76 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.preLoadProducts = void 0;
+const dataSource_1 = require("../config/dataSource");
+const Product_1 = require("../entities/Product");
+const product_repository_1 = require("../repositories/product.repository");
+const productsToPreLoad = [
+    {
+        name: "iPhone 11",
+        price: 699,
+        description: "Experience power and elegance with the iPhone 11: capture stunning moments with its dual-camera system, enjoy exceptional performance, and immerse yourself in a brilliant Liquid Retina display. Discover a world of possibilities in the palm of your hand!",
+        image: "https://d28hi93gr697ol.cloudfront.net/9ef84dda-32dd-4016-7da3-1c0a824fffb4/img/Producto/2ad30421-8829-a476-1028-e20102cb5fe0/iphone-11-black-65972af1513f2.webp",
+        categoryId: 1,
+        stock: 10,
+    },
+    {
+        name: "Apple Vision Pro",
+        price: 999,
+        description: "With Apple Vision Pro, you have an infinite canvas that transforms how you use the apps you love. Arrange apps anywhere and scale them to the perfect size, making the workspace of your dreams a reality — all while staying present in the world around you.",
+        image: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTA8yg-HAqKDg_rswkmU_i_xTkYI9HJdvtJiPyIhsBglndtAA1G17VzddIVjhYQusGOL-XWoxJ-PhG-63fHOGbZmLDyqjLI",
+        categoryId: 2,
+        stock: 10,
+    },
+    {
+        name: "iPad Pro",
+        price: 799,
+        description: "Unleash your creativity and productivity with the iPad Pro: powerful performance, stunning Liquid Retina display, and all-day battery life make the iPad Pro the perfect tool for work and play. Transform your ideas into reality with the iPad Pro.",
+        image: "https://ofertones.com.ar/cdn/shop/products/D_NQ_NP_803992-MLA52988770428_122022-F.jpg?v=1705949937&width=750",
+        categoryId: 3,
+        stock: 10,
+    },
+    {
+        name: "Apple Watch Series 6",
+        price: 399,
+        description: "Stay connected and healthy with the Apple Watch Series 6: track your workouts, monitor your health, and stay in touch with the people and information you care about most. Experience the future of health and wellness with the Apple Watch Series 6.",
+        image: "https://mac-center.com.pe/cdn/shop/products/IMG-5782599_4808a4eb-6132-4b8a-bc59-48aadc807161_823x.jpg?v=1660915002",
+        categoryId: 4,
+        stock: 10,
+    },
+    {
+        name: "AirPods Pro",
+        price: 249,
+        description: "Immerse yourself in sound with the AirPods Pro: active noise cancellation, transparency mode, and customizable fit make the AirPods Pro the perfect companion for music, calls, and everything in between. Elevate your audio experience with the AirPods Pro.",
+        image: "https://plazavea.vteximg.com.br/arquivos/ids/28719943-650-650/20394403.jpg",
+        categoryId: 5,
+        stock: 10,
+    },
+    {
+        name: "HomePod mini",
+        price: 99,
+        description: "Elevate your home audio experience with the HomePod mini: immersive sound, intelligent assistant, and smart home hub make the HomePod mini the perfect addition to your home. Enjoy a world of music, news, and more with the HomePod mini.",
+        image: "https://http2.mlstatic.com/D_NQ_NP_800774-MLA45740145234_042021-O.webp",
+        categoryId: 6,
+        stock: 10,
+    },
+];
+const preLoadProducts = () => __awaiter(void 0, void 0, void 0, function* () {
+    const products = yield product_repository_1.ProductRepository.find();
+    if (!products.length)
+        yield dataSource_1.AppDataSource.createQueryBuilder()
+            .insert()
+            .into(Product_1.Product)
+            .values(productsToPreLoad)
+            .execute();
+    console.log("Products preloaded");
+});
+exports.preLoadProducts = preLoadProducts;
